9cb85e6559c17219958fec84547c1a5987372cfc
Trucbizard365 <97367743+Trucbizar365@users.noreply.github.com>
2026-05-01 20:05:59 +0200

    Test item update

--- src/main/java/net/trucbizard365/valdoriancoremod/item/ModItems.java
+++ src/main/java/net/trucbizard365/valdoriancoremod/item/ModItems.java
@@ -1,6 +1,9 @@
 package net.trucbizard365.valdoriancoremod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,11 +11,17 @@ import net.trucbizard365.valdoriancoremod.ValdorianCoreMode;
 
 public class ModItems {
     public static final Item Test = registerItem(new Item(new Item.Settings()));
+
     private static Item registerItem(Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ValdorianCoreMode.MOD_ID, "test"), item);
     }
+
     public static void registerModItems() {
         ValdorianCoreMode.LOGGER.info("Registering Mod Items for " + ValdorianCoreMode.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(Test);
+        }) ;
     }
 
 }
