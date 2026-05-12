501118c6c1eafc1e225a8a12ce4755ac69adb5f0
Trucbizard365 <97367743+Trucbizar365@users.noreply.github.com>
2026-05-12 20:17:13 +0200

    -Added money

--- src/main/java/net/trucbizard365/valdoriancoremod/item/ModItems.java
+++ src/main/java/net/trucbizard365/valdoriancoremod/item/ModItems.java
@@ -2,7 +2,6 @@ package net.trucbizard365.valdoriancoremod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,10 +9,11 @@ import net.minecraft.util.Identifier;
 import net.trucbizard365.valdoriancoremod.ValdorianCoreMode;
 
 public class ModItems {
-    public static final Item Test = registerItem(new Item(new Item.Settings()));
+    public static final Item Test = registerItem("test", new Item(new Item.Settings()));
+    public static final Item MONEY = registerItem("money", new Item(new Item.Settings()));
 
-    private static Item registerItem(Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(ValdorianCoreMode.MOD_ID, "test"), item);
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ValdorianCoreMode.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
@@ -21,6 +21,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(Test);
+            entries.add(MONEY);
         }) ;
     }
 
